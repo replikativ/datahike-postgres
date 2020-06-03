d359e2624d2c9d31f1381b30c7034012ca5786c8
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2020-06-03 10:59:34 +0200

    use snapshot versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.replikativ/datahike-postgres "0.2.0"
+(defproject io.replikativ/datahike-postgres "0.3.1-SNAPSHOT"
   :description "Datahike with Postgres as data storage."
   :license {:name "Eclipse"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure       "1.10.1"   :scope "provided"]
                  [environ "1.1.0"]
                  [org.clojars.mihaelkonjevic/konserve-pg "0.1.2"]
-                 [io.replikativ/datahike "0.3.0"]]
+                 [io.replikativ/datahike "0.3.1-SNAPSHOT"]]
 
   :aliases {"test-clj"     ["run" "-m" "datahike-postgres.test/core_test-clj"]
             "test-all"     ["do" ["clean"] ["test-clj"]]}
